7d119a679c8332c87a8d60c4ee009e6eb9a764cd
Ed Summers <ehs@pobox.com>
2014-04-19 08:47:33 -0400

    added pie chart

--- opinions.py
+++ opinions.py
@@ -6,9 +6,13 @@ import flask
 from sqlalchemy import func, select, desc
 from flask.ext.sqlalchemy import SQLAlchemy
 
+# create our app
 
 app = flask.Flask(__name__)
 db = SQLAlchemy(app)
+
+
+# database models
 
 class Opinion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,7 +38,6 @@ class Opinion(db.Model):
         d = self.published.strftime("%Y-%m-%d")
         return "%s (%s) <%s>" % (self.name, d, self.pdf_url)
 
-
 class ExternalUrl(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.Text)
@@ -42,7 +45,6 @@ class ExternalUrl(db.Model):
     opinion_id = db.Column(db.Integer, db.ForeignKey('opinion.id'))
     opinion = db.relationship('Opinion', 
         backref=db.backref('external_urls', lazy='dynamic'))
-
 
 class Author(db.Model):
     id = db.Column(db.String(5), primary_key=True)
@@ -53,7 +55,13 @@ class Author(db.Model):
         self.name = name
 
 
+# define some web application routes
+
 @app.route('/')
+def home():
+    return flask.render_template('home.html')
+
+@app.route('/opinions/')
 def opinions():
     opinions = Opinion.query.order_by(Opinion.published.desc()).all()
     return flask.render_template('opinions.html', opinions=opinions)
@@ -73,13 +81,29 @@ def authors():
     rows = db.session.query(q)
     return flask.render_template('authors.html', rows=rows)
 
+@app.route('/authors.csv')
+def authors_csv():
+    q = (select([Author.id, Author.name, func.count(ExternalUrl.id).label("urls")]).
+         where(Author.id == Opinion.author_id).
+         where(Opinion.id == ExternalUrl.opinion_id).
+         group_by(Author.name, Author.id).
+         order_by(desc("urls")))
+    cursor = db.session.query(q)
+    results = ['"id","name",urls']
+    for row in cursor:
+        results.append('"%s","%s",%s' % row)
+
+    return flask.Response('\n'.join(results), mimetype='text/csv')
+
+
+
+# run the webapp
 
 def init():
     db_uri = 'sqlite:///' + os.path.join(app.root_path, 'opinions.db')
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['DEBUG'] = True
     db.create_all()
-
 
 if __name__ == "__main__":
     init()
---
+++ static/js/opinions.js
@@ -0,0 +1,41 @@
+function drawAuthorChart() {
+
+  var width = 500, //960,
+      height = 300,
+      radius = Math.min(width, height) / 2;
+
+  var color = d3.scale.category20();
+
+  var arc = d3.svg.arc()
+      .outerRadius(radius - 10)
+      .innerRadius(radius - 70);
+
+  var pie = d3.layout.pie()
+      .sort(null)
+      .value(function(d) { return d.urls; });
+
+  var svg = d3.select("#author-chart").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  d3.csv("authors.csv", function(error, data) {
+
+    var g = svg.selectAll(".arc")
+        .data(pie(data))
+      .enter().append("g")
+        .attr("class", "arc");
+
+    g.append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color(d.data.id); });
+
+    g.append("text")
+        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d.data.id; });
+  });
+
+}
